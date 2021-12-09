c80c02436461fe4ebf63886f748fb945adfdc07d
notken12 <kendotzhou@gmail.com>
2021-12-09 18:10:47 -0500

    move things into server folder

---
+++ server/games/Player.js
@@ -0,0 +1,16 @@
+var discordApiUtils = require('../../utils/discord-api');
+
+class Player {
+    constructor(id, discordUser) {
+        this.id = id;
+        this.discordUser = discordUser;
+    }
+
+    getDataForClient() {
+        return {
+            discordUser: this.discordUser
+        };
+    }
+}
+
+module.exports = Player;
---
+++ utils/bot-api.js
@@ -0,0 +1,5 @@
+const fetch = require('node-fetch');
+
+function getShardingManagerHost() {
+    
+}
