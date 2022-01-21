fb55172e7a47299add666238c93a5f70b21b7a28
notken12 <kendotzhou@gmail.com>
2022-01-21 18:13:01 -0500

    weird no such file error

--- vite.config.mjs
+++ vite.config.mjs
@@ -29,7 +29,7 @@ var walk = function (dir) {
   var results = [];
   var list = fs.readdirSync(dir);
   list.forEach(function (file) {
-    file = dir + '\\' + file;
+    file = dir + '/' + file;
     var stat = fs.statSync(file);
     if (stat && stat.isDirectory()) {
       /* Recurse into a subdirectory */
