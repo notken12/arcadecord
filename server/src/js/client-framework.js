e48b3a6ace030a3f814ba0247cfeb9fe33dac542
notken12 <kendotzhou@gmail.com>
2022-01-21 23:19:06 -0500

    add app insights

--- server/src/js/client-framework.js
+++ server/src/js/client-framework.js
@@ -3,6 +3,16 @@ import { io } from "socket.io-client";
 import GameFlow from './GameFlow.js';
 import bus from './vue-event-bus.js';
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: 'InstrumentationKey=54ef4b41-2e52-4be2-bf3f-02471829b486;IngestionEndpoint=https://eastus-1.in.applicationinsights.azure.com/'
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView();
+
+
 var socket = io();
 
 class Action {
