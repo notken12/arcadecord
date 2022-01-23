6dd60951d9408a32b0141a7699a39c15e2634cd5
Craftingexpert <dev.velp@gmail.com>
2022-01-22 22:17:04 -0500

    Started developing checkers

---
+++ server/src/games/types/checkers/common.js
@@ -0,0 +1,47 @@
+// Common action models
+
+// Import GameFlow to control game flow
+import GameFlow from '../../GameFlow.js';
+
+
+// An action model is a function...
+// that takes in a Game and an Action (see Game.js and Action.js)
+// and outputs the updated Game if it succeeds, and otherwise outputs false
+
+function boop(game, action) {
+    var i = action.playerIndex;
+    game.data.scores[i]++;
+
+    // You win if you get to 15 points
+    if (game.data.scores[i] >= 15) {
+        GameFlow.end(game, {
+            winner: i
+        });
+    }
+
+    return game;
+}
+
+function endTurn(game, action) {
+    GameFlow.endTurn(game);
+
+    return game;
+}
+
+function ask(game, action) {
+    // Check if the question has a question mark
+    if (action.data.question.indexOf('?') === -1) {
+        return false;
+    }
+    return game;
+}
+
+// Compatibility with browser and node
+
+var exports = {
+    boop,
+    endTurn,
+    ask
+}
+
+export default exports;
