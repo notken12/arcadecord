5bf6d660be1b3e1f02e6e3b736ff492b0271f664
notken12 <kendotzhou@gmail.com>
2022-02-05 15:55:33 -0500

    get rid of scores for chess

--- server/src/components/games/chess/App.vue
+++ server/src/components/games/chess/App.vue
@@ -4,12 +4,6 @@
   <game-view :game="game" :me="me" :hint="hint">
     <!-- Game UI goes in here -->
     <!-- Using the scores-view component, create the score badges -->
-    <scores-view>
-      <!-- Display a score badge for each player -->
-      <template v-slot="scoreView">
-        <score-badge :scoreview="scoreView"></score-badge>
-      </template>
-    </scores-view>
 
     <div class="middle" v-on:click="cellClick">
       <!-- Game UI just for filler -->
