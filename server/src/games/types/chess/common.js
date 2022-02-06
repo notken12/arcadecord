a6788aab0bfbf5891b892a57ef63b24459b5f4bf
notken12 <kendotzhou@gmail.com>
2022-02-05 21:29:46 -0500

    rewrite legalchessmoves and move to common.js

--- server/src/games/types/chess/common.js
+++ server/src/games/types/chess/common.js
@@ -15,6 +15,333 @@ var ranks = "87654321";
 //   GameFlow = window.GameFlow;
 // }
 
+function recurse(board, moves, piece, location, offset, singleMove) {
+  let { rank, file } = location
+  let newFile = file + offset[0]
+  let newRank = rank + offset[1]
+
+  if (newFile < 0 || newFile > 7 || newRank < 0 || newRank > 7) {
+    // Out of bounds
+    return moves
+  }
+
+  // Add move
+  moves.push({
+    from: [piece.file, piece.rank],
+    to: [newFile, newRank],
+  })
+
+  if (singleMove) {
+    // Piece can only move one space
+    // End recursion
+
+    return moves
+  }
+
+  if (
+    board.find(
+      (piece) => piece.rank === newRank && piece.file === newFile
+    )
+  ) {
+    // There's a piece here
+    // End recursion
+
+    return moves
+  } else {
+    // No piece here
+    // Recurse
+    return recurse(
+      board,
+      moves,
+      piece,
+      { rank: newRank, file: newFile },
+      offset
+    )
+  }
+}
+
+function getMoves(board, piece /*{color: 0 white 1 black, file: 0-7, rank: 0-7, type: lowercase letter, id, moved: bool}*/) {
+  function isOutOfBounds(file, rank) {
+    return rank < 0 || rank > 7 || file < 0 || file > 7
+  }
+
+  const offsets = {
+    // [file, rank]
+    r: [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ],
+    b: [
+      [1, 1],
+      [-1, -1],
+      [1, -1],
+      [-1, 1],
+    ],
+    q: [
+      [1, 1],
+      [-1, -1],
+      [1, -1],
+      [-1, 1],
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ],
+    k: [
+      [1, 1],
+      [-1, -1],
+      [1, -1],
+      [-1, 1],
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ],
+  }
+
+  let singleMovePieces = ['k']
+
+  let moves = []
+
+  let offset = offsets[piece.type]
+  if (offset) {
+    recurse(
+      board,
+      moves,
+      piece,
+      { rank: piece.rank, file: piece.file },
+      offset,
+      singleMovePieces.includes(piece.type)
+    )
+  } else {
+    // One of the oddball pieces
+    // Pawn, Knight
+    switch (piece.type) {
+      case 'p':
+        // Pawn
+        let forward = piece.color === 0 ? 1 : -1
+
+        let newFile = piece.file
+        let newRank1 = piece.rank + forward
+
+        let newRank2 = piece.rank + 2 * forward
+
+        if (isOutOfBounds(newFile, newRank1)) {
+          // Out of bounds
+          return moves
+        }
+
+        // Moving 1 space forward is valid
+        moves.push({
+          from: [piece.file, piece.rank],
+          to: [newFile, newRank1],
+        })
+
+        if (piece.color === 0) {
+          // White Pawn
+          if (piece.rank === 1) {
+            // Pawn can move 2 spaces
+            moves.push({
+              from: [piece.file, piece.rank],
+              to: [newFile, newRank2],
+              double: true,
+            })
+          }
+        } else {
+          // Black Pawn
+          if (piece.rank === 6) {
+            // Pawn can move 2 spaces
+            moves.push({
+              from: [piece.file, piece.rank],
+              to: [newFile, newRank2],
+              double: true,
+            })
+          }
+        }
+
+        // Pawn can capture diagonally
+        let newFile1 = piece.file + 1
+        let newFile2 = piece.file - 1
+
+        if (!isOutOfBounds(newFile1, newRank1)) {
+          // Right
+          if (
+            board.find(
+              (piece) => piece.rank === newRank1 && piece.file === newFile1
+            )
+          ) {
+            // There's a piece here
+            moves.push({
+              from: [piece.file, piece.rank],
+              to: [newFile1, newRank1],
+            })
+          }
+        }
+
+        if (!isOutOfBounds(newFile2, newRank1)) {
+          // Left
+          if (
+            board.find(
+              (piece) => piece.rank === newRank1 && piece.file === newFile2
+            )
+          ) {
+            // There's a piece here
+            moves.push({
+              from: [piece.file, piece.rank],
+              to: [newFile2, newRank1],
+            })
+          }
+        }
+        break
+      case 'n':
+        // Knight
+        let knightOffsets = [
+          [1, 2],
+          [2, 1],
+          [2, -1],
+          [1, -2],
+          [-1, -2],
+          [-2, -1],
+          [-2, 1],
+          [-1, 2],
+        ]
+
+        for (let offset of knightOffsets) {
+          let newFile = piece.file + offset[0]
+          let newRank = piece.rank + offset[1]
+
+          if (newFile < 0 || newFile > 7 || newRank < 0 || newRank > 7) {
+            // Out of bounds
+            continue
+          }
+
+          // Add move
+          moves.push({
+            from: [piece.file, piece.rank],
+            to: [newFile, newRank],
+          })
+        }
+        break
+    }
+  }
+
+  // Additional branching for castling
+  if (piece.type === 'k') {
+    let king = piece
+    // Castling is only valid if the king and rook haven't moved
+    // There must not be any pieces between the king and the rook
+    // The king may not be in check
+    // The king may not pass through a square that is under attack
+    // It is okay if the rook is under attack
+
+    if (king.moved) {
+      // King has moved
+      return moves
+    }
+
+    for (let i = 0; i < 2; i++) {
+      // 0 is queens side, 1 is kings side
+      let rook = board.find(
+        (piece) =>
+          piece.type === 'r' &&
+          piece.color === king.color &&
+          piece.rank === king.rank &&
+          piece.file === (i === 0 ? 0 : 7)
+      )
+
+      if (!rook) {
+        // No rook
+        continue
+      }
+
+      if (rook.moved) {
+        // Rook has moved
+        continue
+      }
+
+      // Check if there are pieces between the king and the rook
+      let piecesBetween = []
+      let file = king.file
+      let rank = king.rank
+      while (file !== rook.file && piecesBetween.length < 1) {
+        file = i === 0 ? file + 1 : file - 1
+        piecesBetween.push(board.find(
+          (piece) => piece.rank === rank && piece.file === file
+        ))
+      }
+
+      if (piecesBetween.length > 0) {
+        // There are pieces between the king and the rook
+        continue
+      }
+
+      // Check if the king is in check
+      if (isInCheck(board, king)) {
+        // King is in check
+        continue
+      }
+
+      // Check if the king passes through a square that is under attack
+      let kingMoves;
+
+      if (i === 0) {
+        kingMoves = [
+          [king.file - 1, king.rank],
+          [king.file - 2, king.rank],
+        ]
+      } else {
+        kingMoves = [
+          [king.file + 1, king.rank],
+          [king.file + 2, king.rank],
+        ]
+      }
+
+      let attackedSquares = []
+      for (let move of kingMoves) {
+        if (isUnderAttack(board, move, king.color)) {
+          attackedSquares.push(move)
+        }
+      }
+
+      if (attackedSquares.length > 0) {
+        // King passes through a square that is under attack
+        continue
+      }
+
+      // King and rook are clear to castle
+      moves.push({
+        from: [king.file, king.rank],
+        to: [i === 0 ? 2 : 6, king.rank],
+        castle: i,
+      })
+    }
+  }
+
+  return moves
+}
+
+function isUnderAttack(board, square, friendlyColor) {
+  // Returns true if the square is under attack by a piece of the opposite color
+  let pieces = board.filter((piece) => piece.color !== friendlyColor)
+
+  for (let piece of pieces) {
+    let moves = getMoves(board, piece)
+    for (let move of moves) {
+      if (move.to[0] === square[0] && move.to[1] === square[1]) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+function isInCheck(board, king) {
+  return isUnderAttack(board, [king.file, king.rank], king.color)
+}
+
 function isGameOver(board, player) {
 
   if (player == 1) {//White Pieces
