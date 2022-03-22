39db96a5a7fcf867538cf174de80b4278b8d787d
Crazexpert <dev.velp@gmail.com>
2022-03-22 02:53:26 +0000

    almost

--- server/src/components/games/knockout/GameCanvas.vue
+++ server/src/components/games/knockout/GameCanvas.vue
@@ -12,7 +12,7 @@ const {
   $endAnimation,
 } = useFacade()
 
-import Matter from 'matter-js'
+/*import Matter from 'matter-js'
 
 //just the boilerplate that matter js has provided for now
 
@@ -25,16 +25,107 @@ var Engine = Matter.Engine,
 var engine = Engine.create()
 
 // create two boxes and a ground
-
+var boxA = Bodies.rectangle(400, 200, 80, 80)
+var boxB = Bodies.rectangle(450, 50, 80, 80) 
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true })
 
 // add all of the bodies to the world
 Composite.add(engine.world, [boxA, boxB, ground])
 
-const canvas = ref(null)
 engine.gravity.y = 0;
 
 Render.run(Render.create({ element: canvas.value, engine }))
-Runner.run(Runner.create(), engine)
+Runner.run(Runner.create(), engine)*/
+
+
+const canvas = this.$refs.canvas;
+const ctx = canvas.getContext('2d');
+var width, height, dummyRadius = 30, dummies = [];
+
+var collide = (c1, c2) => (c2.x - c1.x) ** 2 + (c2.y - c1.y) ** 2 <= (c1.r + c2.r) ** 2;
+
+class Dummy { 
+    constructor(x, y, faceDir, playerIndex, moveDir, fallen) {
+        this.x = x; //x and y relative to ice size
+        this.y = y;
+        this.moveDir = moveDir || undefined; //vector
+        this.faceDir = faceDir; //angle in degrees
+        this.playerIndex = playerIndex;
+        this.fallen = fallen || false;
+        this.sussy = "ඞ";
+    }
+}
+function randRange(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+for (var i = 0; i < 8; i++) {
+    dummies.push( // don't want to have x or y be at the very edge of the ice
+       new Dummy(randRange(10, 90), randRange(10, 90), randRange(0, 360), (i < 4) ? 1 : 0)
+    );
+}
+
+
+function drawDummy(color, lx, ly, mobile) {
+    // x and y are based on 0 - 100 relative to ice
+    // radius is same for all
+    ctx.beginPath()
+    ctx.fillStyle = color;
+    var x, y;
+    if (mobile) {
+      x = width / 2 - height / 4 + padding + (height / 2 - padding * 2) * lx / 100;
+      y = height / 4 + padding + (height / 2 - padding * 2) * ly / 100;
+    } else {
+      x = width / 4 + padding + (width / 2 - padding * 2) * lx / 100;
+      y = height / 2 - width / 4 + padding + (width / 2 - padding * 2) * ly / 100;
+    }
+    
+    ctx.arc(x, y, dummyRadius, 0, 2 * Math.PI);
+    ctx.fill();
+    
+}
+
+
+function draw() {
+    
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+  ctx.clearRect(0, 0, width, height);
+
+  // drawing the ice is important because the dummies' position will be relative to it
+  // so we'll use the ratios of the device to know that position
+  // also the ice is square
+  ctx.fillStyle = 'lightBlue';
+  var mobile = false; //height greater
+  
+    
+  if (height > width) {
+    // width is screen width - padding
+    // height is half of screen height
+    padding = height / 16
+    ctx.fillRect(width / 2 - height / 4 + padding, height / 4 + padding, height / 2 - padding * 2, height / 2 - padding * 2);
+    mobile = true;  
+  } else {
+    padding = width / 16;
+    ctx.fillRect(width / 4 + padding, height / 2 - width / 4 + padding, width / 2 - padding * 2, width / 2 - padding * 2); 
+  }
+  dummies.forEach(dum => {
+      drawDummy(dum.playerIndex ? 'blue' : 'black', dum.x, dum.y, mobile);
+      dummies.forEach(compare => {
+          
+      });
+  })
+  
+  requestAnimationFrame(draw);
+
+}
+document.body.appendChild(canvas);
+draw();
+
+
 </script>
 
 <template>
--- server/src/games/types/knockout/common.js
+++ server/src/games/types/knockout/common.js
@@ -1,87 +1,84 @@
 import GameFlow from '../../GameFlow.js'
 
-class Dummy {
-  constructor(x, y, faceDir, playerIndex, moveDir, fallen) {
-    this.x = x //x and y relative to ice size
-    this.y = y
-    this.moveDir = moveDir || undefined //vector
-    this.faceDir = faceDir //angle in degrees
-    this.playerIndex = playerIndex
-    this.fallen = fallen || false
-    this.sussy = 'ඞ'
-  }
+class Dummy { 
+    constructor(x, y, faceDir, playerIndex, moveDir, fallen) {
+        this.x = x; //x and y relative to ice size
+        this.y = y;
+        this.moveDir = moveDir || undefined; //vector
+        this.faceDir = faceDir; //angle in degrees
+        this.playerIndex = playerIndex;
+        this.fallen = fallen || false;
+        this.sussy = "ඞ";
+    }
 }
 
 class Ice {
-  static decrease = 5
-  constructor(size) {
-    this.size = size
-  }
+    static decrease = 5;
+    constructor(size) {
+        this.size = size;
+    }
 }
 
 async function setDirections(game, action) {
-  action.data.directions.forEach((direction, index) => {
-    if (direction) {
-      game.data.dummies[
-        index + !action.userId * 4 //player 1's indexes are 0 to 3
-      ].moveDir = direction
+    action.data.directions.forEach((direction, index) => {
+        if (direction) {
+            game.data.dummies[
+                index + !action.userId * 4 //player 1's indexes are 0 to 3
+            ].moveDir = direction;
+        }
+
+    });
+    await GameFlow.end(game, {winner:checkWinner(game)});
+    var moving, fallen; //was gonna use arrays but numbers is faster
+    for (var i = 0; i < game.data.dummies.length; i++) {
+        cur = game.data.dummies[i]
+        if (cur.moveDir != undefined) moving++;
+        if (cur.fallen == true) fallen++;
     }
-  })
-  await checkWinner(game)
-  var moving, fallen //was gonna use arrays but numbers is faster
-  for (var i = 0; i < game.data.dummies.length; i++) {
-    cur = game.data.dummies[i]
-    if (cur.moveDir != undefined) moving++
-    if (cur.fallen == true) fallen++
-  }
-  if (moving == 8 - fallen) {
-    // do not switch turns, push everything and decrease size
-    // everything needs to be moved in client
-    game.data.ice.size -= Ice.decrease
-  } else {
-    await GameFlow.endTurn(game)
-  }
+    if (moving == 8 - fallen) {
+        // do not switch turns, push everything and decrease size
+        // everything needs to be moved in client
+        game.data.ice.size -= Ice.decrease;
+    } else {
+        await GameFlow.endTurn(game);
+    }
 }
 
-async function checkWinner(game) {
-  var p1fallen, p0fallen, winner
-  for (var i = 0; i < game.data.dummies.length; i++) {
-    cur = game.data.dummies[i]
-    if (!cur.fallen) continue //stop for loop if hasn't fallen
-    if (cur.playerIndex) p1fallen++
-    //player index is either 0 or 1
-    else p0fallen++
-  }
-  if (p1fallen == 4) winner = 1
-  else if (p0fallen == 4) winner = 0
-  else if (p1fallen == 4 && p0fallen) return 'draw' //??? draw?? how do i do that
-  await GameFlow.end(game, { winner })
+function checkWinner(game) {
+    var p1, p0, winner;
+    for (var i = 0; i < game.data.dummies.length; i++) {
+        cur = game.data.dummies[i]
+        if (!cur.fallen) continue; //stop for loop if hasn't fallen
+        if (cur.playerIndex) p1++; //player index is either 0 or 1
+        else p0++;
+    }
+    if (p1 == 4) winner = 1; //p1
+    else if (p0 == 4) winner = 0; //p2
+    else if (p1 == 4 && p0 == 4) winner = -1; // draw cause they can all fall 
+    return winner;
+
 }
 
 function spawn() {
-  var ice = new Ice(100)
-  dummies = []
+    var ice = new Ice(100);
+    dummies = [];
 
-  //have to check if they collide with the other ones before they are pushed :/
-  //GRRRRRRR ඞඞඞඞඞඞ
-  //it's ok i'll fix it later maybe in the client
+    
+    //have to check if they collide with the other ones before they are pushed :/
+    //GRRRRRRR ඞඞඞඞඞඞ
+    //it's ok i'll fix it later maybe in the client
 
-  for (var i = 0; i < 8; i++) {
-    dummies.push(
-      // don't want to have x or y be at the very edge of the ice
-      new Dummy(
-        randRange(10, 90),
-        randRange(10, 90),
-        randRange(0, 360),
-        i < 4 ? 1 : 0
-      )
-    )
-  }
-  return { ice, dummies }
+
+    for (var i = 0; i < 8; i++) {
+        dummies.push( // don't want to have x or y be at the very edge of the ice
+            new Dummy(randRange(10, 90), randRange(10, 90), randRange(0, 360), (i < 4) ? 1 : 0)
+        );
+    }
+    return {ice, dummies};
 }
 
 function randRange(min, max) {
-  return Math.random() * (max - min) + min
+    return Math.random() * (max - min) + min;
 }
 
-export default { Ice, Dummy, setDirections, spawn }
+export default {Ice, Dummy, setDirections, spawn}
