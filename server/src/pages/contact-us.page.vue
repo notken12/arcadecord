3da2403332dcedf65018f6283ac0c0f9c8cc551e
Seth <sethparcell@gmail.com>
2022-04-11 18:05:26 -0400

    added support-us page

---
+++ server/src/pages/contact-us.page.vue
@@ -0,0 +1,40 @@
+<script setup>
+import Header from 'components/index/Header.vue'
+import Banner from 'components/index/Banner.vue'
+import Content from 'components/index/Content.vue'
+import Footer from 'components/index/Footer.vue'
+import Button from 'components/index/Button.vue'
+import GameDisplay from 'components/index/GameDisplay.vue'
+</script>
+
+<template>
+    <div class="container">
+        <Header></Header>
+        <Content id="features">
+          <section>
+          
+          </section>
+      </Content>
+      <Footer></Footer>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0px;
+  width: 100%;
+  height: 100%;
+  background: #1b1b1f;
+}
+
+a:hover {
+  text-decoration: none;
+}
+</style>
+
+<style lang="scss">
+@use 'scss/base/pages';
+</style>
