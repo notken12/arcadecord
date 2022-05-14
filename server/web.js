a44814bbe215618d55bcb6d75e8fcd104abdba58
Ken Zhou <kendotzhou@gmail.com>
2022-05-14 12:46:06 -0400

    licensesnip

--- server/web.js
+++ server/web.js
@@ -1,3 +1,12 @@
+// web.js - Arcadecord
+//
+// Copyright (C) 2022 Ken Zhou
+//
+// This file is part of Arcadecord.
+//
+// Arcadecord can not be copied and/or distributed
+// without the express permission of Ken Zhou.
+
 let start = Date.now();
 
 import dotenv from 'dotenv';
--- server/web/config.js
+++ server/web/config.js
@@ -1,3 +1,12 @@
+// config.js - Arcadecord
+//
+// Copyright (C) 2022 Ken Zhou
+//
+// This file is part of Arcadecord.
+//
+// Arcadecord can not be copied and/or distributed
+// without the express permission of Ken Zhou.
+
 export function loadWebHostConfig() {
   const host = {
     gameServerUrl: process.env.GAME_SERVER_URL,
