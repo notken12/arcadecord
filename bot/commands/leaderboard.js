2aa6e37ae94cd012c0a25c3ce172d4337c92796e
Craftingexpert <dev.velp@gmail.com>
2022-07-07 16:10:18 -0400

    hi

--- bot/commands/leaderboard.js
+++ bot/commands/leaderboard.js
@@ -1,4 +1,4 @@
-// ping.js - Arcadecord
+// leaderboard.js - Arcadecord
 //
 // Copyright (C) 2022 Ken Zhou
 //
@@ -12,29 +12,29 @@ import db from '../../db/db2.js';
 import { gameTypes } from '../../server/src/games/game-types.js';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('leaderboard')
-    .setDescription("Check this server's leaderboard and stats!"),
-  async execute(_config, interaction) {
-    var server = await db.servers.getById(interaction.guildId);
-    if (server == null) {
-      server = await db.servers.create({ _id: interaction.guildId });
-    }
-    var msg =
-      '```cpp\n' +
-      `Arcadecord stats for ${interaction.guild.name}\n\n` +
-      `Games played per type:  \n`;
-    for (var g in gameTypes) {
-      var game = gameTypes[g];
-      var stats = server.stats.games.get(game.options.typeId),
-        gamesPlayed = 0;
-      if (stats) gamesPlayed = stats.gamesPlayed;
-      if (!game.hidden)
-        msg += '  ↳ ' + game.options.name + ': ' + gamesPlayed + '\n';
-    }
-    msg += `\nGames played overall: ${server.gamesPlayed}`;
+    data: new SlashCommandBuilder()
+        .setName('leaderboard')
+        .setDescription("Check this server's leaderboard and stats!"),
+    async execute(_config, interaction) {
+        var server = await db.servers.getById(interaction.guildId);
+        if (server == null) {
+            server = await db.servers.create({ _id: interaction.guildId });
+        }
+        var msg =
+            '```cpp\n' +
+            `Arcadecord stats for ${interaction.guild.name}\n\n` +
+            `Games played per type:  \n`;
+        for (var g in gameTypes) {
+            var game = gameTypes[g];
+            var stats = server.stats.games.get(game.options.typeId),
+                gamesPlayed = 0;
+            if (stats) gamesPlayed = stats.gamesPlayed;
+            if (!game.hidden)
+                msg += '  ↳ ' + game.options.name + ': ' + gamesPlayed + '\n';
+        }
+        msg += `\nGames played overall: ${server.gamesPlayed}`;
 
-    msg += '```';
-    await interaction.reply(msg);
-  },
+        msg += '```';
+        await interaction.reply(msg);
+    },
 };
